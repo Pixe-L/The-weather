4a203d1aed0fc7506b8d63dbc15afccbfd41444b
Arturo <arturcas2@hotmail.com>
2021-08-04 14:37:28 -0500

    weather app first

---
+++ main.js
@@ -0,0 +1,7 @@
+const api = {
+	key: '3c220d15f00f13c79b4b00a3564e7f1d',
+	url: 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}',
+};
+
+const form = document.getElementById('search-form');
+const search = document.getElementById('search-form');
