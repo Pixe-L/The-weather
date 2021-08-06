c6c244b1cf8ccda20f8c3924e8d0ade10f17e88d
Arturo <arturcas2@hotmail.com>
2021-08-06 15:01:40 -0500

    modified index

--- js/main.js
+++ js/main.js
@@ -3,25 +3,48 @@ const api = {
 	url: 'http://api.openweathermap.org/data/2.5/weather',
 };
 
+const card = document.getElementById('card');
+
 const city = document.getElementById('city');
 const date = document.getElementById('date');
-const tempImage = document.getElementById('tempImage');
+const tempImg = document.getElementById('temp-img');
 const temp = document.getElementById('temp');
 const weather = document.getElementById('weather');
 const range = document.getElementById('range');
+
+function updateImages(data) {
+	const temp = toCelsius(data.main.temp);
+	let src = './images/termometro.svg';
+	if (temp > 26) {
+		src = './images/high-temperature.svg';
+	} else if (temp < 20) {
+		src = './images/low-temperature.svg';
+	}
+	tempImg.src = src;
+}
 
 async function search(query) {
 	try {
 		const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
 		const data = await response.json();
-
-		console.log(data);
+		card.style.display = 'block';
+		city.innerHTML = `${data.name}, ${data.sys.country}`;
+		date.innerHTML = new Date().toLocaleDateString();
+		temp.innerHTML = `${toCelsius(data.main.temp)}°C`;
+		weather.innerHTML = data.weather[0].description;
+		range.innerHTML = `${toCelsius(data.main.temp_min)}°C / ${toCelsius(
+			data.main.temp_max
+		)}°C`;
+		updateImages(data);
 	} catch (error) {
 		console.log(error);
 		alert('Hubo un error');
 	}
 }
 
+function toCelsius(kelvin) {
+	return Math.round(kelvin - 273.15);
+}
 function onSubmit(event) {
 	event.preventDefault();
 	search(searchbox.value);
@@ -30,4 +53,4 @@ function onSubmit(event) {
 const searchform = document.getElementById('search-form');
 const searchbox = document.getElementById('searchbox');
 
-form.addEventListener('submit', onSubmit, true);
+searchform.addEventListener('submit', onSubmit, true);
